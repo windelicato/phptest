b6b4a98d09293a4d49512cdf8213fab6fe4eab6c
wei001 <wei001@bucknell.edu>
2013-09-17 13:01:48 -0400

    index up

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>
+    $(document).ready(function(e){
+
+    }); 
+</script>
+</head>
+<body>
+
+    <!-- INPUT -->
+    <form id="input" method="get">
+    My custom text: <input id="text" type="text" name="text"><br>
+    <input id="submit-button" type="submit" value="Submit">
+    </form><br>
+
+    <br>
+
+    <div id="response">        
+    <!-- RESPONSE HERE -->
+    </div>
+
+
+</body>
+</html>
